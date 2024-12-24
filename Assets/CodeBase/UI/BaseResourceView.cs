d3fb894c6988f584508d34fe9ce1406d2cf5da82
Olsney <104034829+Olsney@users.noreply.github.com>
2024-12-24 21:25:38 +0200

    UI fix

--- Assets/CodeBase/UI/BaseResourceView.cs
+++ Assets/CodeBase/UI/BaseResourceView.cs
@@ -9,10 +9,20 @@ namespace CodeBase.UI
     public class BaseResourceView : MonoBehaviour
     {
         [SerializeField] private CollectorsBase _collectorsBase;
-        [SerializeField] private TMP_Text _textMesh; 
+        [SerializeField] private TMP_Text _textMesh;
+        private Camera _mainCamera;
+
+        private void Start() => 
+            _mainCamera = Camera.main;
 
         private void OnEnable() => 
             _collectorsBase.ResourcesCollected += OnResourcesCollected;
+
+        private void LateUpdate() => 
+            ConfigureLooking();
+
+        private void ConfigureLooking() => 
+            transform.LookAt(transform.position + _mainCamera.transform.forward);
 
         private void OnResourcesCollected(int count) => 
             _textMesh.text = $"{count}";
